dbd2e22e1abc1779b5aaf503367dd64049d5f4e9
Arseniy Lebedev <nakendlom@gmail.com>
2019-09-08 18:08:39 +0300

    борьба с кэшем

--- public/_inc/header.php
+++ public/_inc/header.php
@@ -15,19 +15,19 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css' rel='stylesheet' type='text/css'>
 
-    <link href='/_assets/css/style.css' rel='stylesheet' type='text/css'>
+    <link href='/_asset/css/style.css?v=2' rel='stylesheet' type='text/css'>
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <script src="/_assets/js/lookup.js" crossorigin="anonymous"></script>
+    <script src="/_asset/js/lookup.js?v=2" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <header>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top">            
-            <a class="navbar-brand" href="/"><img src="/_assets/img/site_logo.gif" style="height:38px"></a>
+            <a class="navbar-brand" href="/"><img src="/_asset/img/site_logo.gif" style="height:38px"></a>
             
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav mr-auto">
