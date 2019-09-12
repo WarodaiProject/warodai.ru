98c40a6f0a7d9ce90a58241377e2c9231cad8b2e
Arseniy Lebedev <nakendlom@gmail.com>
2019-09-12 22:37:25 +0300

    смена версий

--- public/_inc/header.php
+++ public/_inc/header.php
@@ -19,13 +19,13 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css' rel='stylesheet' type='text/css'>
 
-    <link href='/_asset/css/style.css?v=2' rel='stylesheet' type='text/css'>
+    <link href='/_asset/css/style.css?v=3' rel='stylesheet' type='text/css'>
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <script src="/_asset/js/lookup.js?v=2" crossorigin="anonymous"></script>
+    <script src="/_asset/js/lookup.js?v=3" crossorigin="anonymous"></script>
 </head>
 
 <body>
