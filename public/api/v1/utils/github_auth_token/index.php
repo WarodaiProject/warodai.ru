f8090de48516ea03b512faae732439253aa84d0c
Arseniy Lebedev <nakendlom@gmail.com>
2019-09-15 23:13:06 +0300

    Изменена прокся для github

--- public/api/v1/utils/github_auth_token/index.php
+++ public/api/v1/utils/github_auth_token/index.php
@@ -1,33 +1,28 @@
 <?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Max-Age: 1000');
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Max-Age: 1000');
+    $ch = curl_init('https://github.com/login/oauth/access_token');
 
+    $query = $_POST;
 
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');  
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($query));
 
-$ch = curl_init( $_GET['url'] );
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Accept: application/json'
+    ));
 
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');  
-curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query($_POST) );
+    curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+    curl_setopt( $ch, CURLOPT_HEADER, true );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
 
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Accept: application/json'
-));
+    $response = curl_exec( $ch );
+    $status = curl_getinfo( $ch ); 
+    $header = trim(substr($response, 0, $status['header_size']));
+    $body = substr($response, $status['header_size']);
 
-curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-curl_setopt( $ch, CURLOPT_HEADER, true );
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-
-$response = curl_exec( $ch );
-$status = curl_getinfo( $ch ); 
-$header = trim(substr($response, 0, $status['header_size']));
-$body = substr($response, $status['header_size']);
-
-header('');
-
-curl_close( $ch );    
-print($body);
-
-
+    curl_close($ch);
+    print($body);
 ?>
