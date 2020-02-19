dab4831bf4ef968480e99b767ff665918c06dc6e
nakendlom <nakendlom@gmail.com>
2020-02-20 02:06:32 +0300

    сменена версия

--- public/_inc/header.php
+++ public/_inc/header.php
@@ -26,9 +26,9 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     
-    <script src="/_asset/js/config.js.php?v=3" crossorigin="anonymous"></script>
-    <script src="/_asset/js/auth.js?v=3" crossorigin="anonymous"></script>
-    <script src="/_asset/js/lookup.js?v=3" crossorigin="anonymous"></script>
+    <script src="/_asset/js/config.js.php?v=4" crossorigin="anonymous"></script>
+    <script src="/_asset/js/auth.js?v=4" crossorigin="anonymous"></script>
+    <script src="/_asset/js/lookup.js?v=4" crossorigin="anonymous"></script>
     <script src="/_asset/js/diff.js" crossorigin="anonymous"></script>
 </head>
 
