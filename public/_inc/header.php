de8e66ffaac37f78488ab6f9001c10c6b00213f4
nakendlom <nakendlom@gmail.com>
2022-01-15 12:03:25 +0300

    Завендорены все зависимости

--- public/_inc/header.php
+++ public/_inc/header.php
@@ -17,14 +17,14 @@
 
     <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon.png">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/vendor/bootstrap-4.4.1-dist/css/bootstrap.min.css">
+    <link href='/vendor/fontawesome-free-5.15.4-web/css/all.min.css' rel='stylesheet' type='text/css'>
 
     <link href='/_asset/css/style.css?v=3' rel='stylesheet' type='text/css'>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="/vendor/jquery/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+    <script src="/vendor/popper/popper.min.js"></script>
+    <script src="/vendor/bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
     
     <script src="/_asset/js/config.js.php?v=4" crossorigin="anonymous"></script>
     <script src="/_asset/js/auth.js?v=4" crossorigin="anonymous"></script>
